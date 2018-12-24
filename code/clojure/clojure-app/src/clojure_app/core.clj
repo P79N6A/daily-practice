1c8cb3d85fd3860ee0ea3190c1713eb43f33f4ae
rockdragon <moyerock@gmail.com>
2018-12-24 16:23:54 +0800

    feat: add 2 fn
    Change-Id: I4362989c5d5491990f42493d8a63b0e658ceaf0d

--- code/clojure/clojure-app/src/clojure_app/core.clj
+++ code/clojure/clojure-app/src/clojure_app/core.clj
@@ -2,10 +2,21 @@
   (:gen-class)
   (:require [clojure.core.reducers :as r]))
 
+(def numbers (into [] (range 0 10000000)))
+
+(def counts {"apple" 2 "orange" 1})
+
+(def pages ["one potato two potato three potato four"
+            "five potato six potato seven potato more"])
+
+(defn get-words [text] (re-seq #"\w+" text))
+
 (defn parallel-sum [numbers]
   (r/fold + numbers))
 
-(def numbers (into [] (range 0 10000000)))
+(defn count-words_parallel [pages]
+  (reduce (partial merge-with +)
+          (pmap #(frequencies (get-words %)) pages)))
 
 (defn -main
   "I don't do a whole lot ... yet."
