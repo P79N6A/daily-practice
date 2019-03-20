aed21700e5574288a10fa213a3ea3df85ade4916
Locke <moyerock@gmail.com>
2019-03-20 22:41:36 +0800

    feat: O(log N)

--- code/python/algorithms/dp.py
+++ code/python/algorithms/dp.py
@@ -1,13 +1,24 @@
+def find_insertion(sub_seq, start, end, value):
+    if sub_seq[end] < value:
+        return end+1
+    while start < end:
+        middle = int(start + (end - start)/2)
+        if sub_seq[middle] <= value:
+            start = middle + 1
+        else:
+            end = middle
+    return start
+
 def longest_sub_seq(seq):
     n = len(seq)
-    d = [1] * n
-    max = 1
+    sub_seq = [0] * n
+    max = 0
+    sub_seq[1] = seq[0]
     for i in range(0, n):
-        for j in range(0, i):
-            if seq[j] <= seq[i] and d[j]+1 > d[i]:
-                d[i] = d[j] + 1
-        if d[i] > max:
-            max = d[i]
+        pos = find_insertion(sub_seq, 0, max, seq[i])
+        sub_seq[pos] = seq[i]
+        if max < pos:
+            max = pos
     return max
 
 
