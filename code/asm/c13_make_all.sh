e1a359e325ea86966b2fc284bdbbb1e20d44fb6b
Locke <moyerock@gmail.com>
2019-06-08 21:15:38 +0800

    feat: make all about chapter 13

---
+++ code/asm/c13_make_all.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+nasm -f bin c13_mbr.asm -o c13_mbr.bin
+nasm -f bin c13_core.asm -o c13_core.bin
+nasm -f bin c13_user.asm -o c13_user.bin
+
+python fixvhdwr.py c13_mbr.bin ~/VirtualBox\ VMs/LearnASM/LearnASM.vhd 0
+python fixvhdwr.py c13_core.bin ~/VirtualBox\ VMs/LearnASM/LearnASM.vhd 1
+python fixvhdwr.py c13_user.bin ~/VirtualBox\ VMs/LearnASM/LearnASM.vhd 50
