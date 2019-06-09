74f83c08826861744f44c626adca78d47104ae0e
Locke <moyerock@gmail.com>
2019-06-09 11:15:57 +0800

    chore: fail to boot

--- code/asm/c13_make_all.sh
+++ code/asm/c13_make_all.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-nasm -f bin c13_1_mbr.asm -o c13_mbr.bin
-nasm -f bin c13_2_core.asm -o c13_core.bin
-nasm -f bin c13_3_user.asm -o c13_user.bin
+nasm -f bin c13_1_mbr.asm -o c13_1_mbr.bin
+nasm -f bin c13_2_core.asm -o c13_2_core.bin
+nasm -f bin c13_3_user.asm -o c13_3_user.bin
 
-python fixvhdwr.py c13_mbr.bin ~/VirtualBox\ VMs/LearnASM/LearnASM.vhd 0
-python fixvhdwr.py c13_core.bin ~/VirtualBox\ VMs/LearnASM/LearnASM.vhd 1
-python fixvhdwr.py c13_user.bin ~/VirtualBox\ VMs/LearnASM/LearnASM.vhd 50
-python fixvhdwr.py c13_diskdata.txt ~/VirtualBox\ VMs/LearnASM/LearnASM.vhd 100
+# dd if=c13_1_mbr.bin of=~/VirtualBox\ VMs/LearnASM/LearnASMNew.vhd conv=notrunc
+# dd if=c13_2_core.bin of=~/VirtualBox\ VMs/LearnASM/LearnASMNew.vhd seek=1  conv=notrunc
+# dd if=c13_3_user.bin of=~/VirtualBox\ VMs/LearnASM/LearnASMNew.vhd seek=50  conv=notrunc
+# dd if=c13_diskdata.txt of=~/VirtualBox\ VMs/LearnASM/LearnASMNew.vhd seek=100  conv=notrunc
+
+python fixvhdwr.py c13_1_mbr.bin ~/VirtualBox\ VMs/LearnASM/LearnASMNew.vhd 0
+python fixvhdwr.py c13_2_core.bin ~/VirtualBox\ VMs/LearnASM/LearnASMNew.vhd 1
+python fixvhdwr.py c13_3_user.bin ~/VirtualBox\ VMs/LearnASM/LearnASMNew.vhd 50
+python fixvhdwr.py c13_diskdata.txt ~/VirtualBox\ VMs/LearnASM/LearnASMNew.vhd 100
