39051853643d8a5e606425625e070f96aacf8de2
Locke <moyerock@gmail.com>
2019-06-09 10:37:58 +0800

    feat: x86 asm book source code

--- code/asm/c13_make_all.sh
+++ code/asm/c13_make_all.sh
@@ -7,3 +7,4 @@ nasm -f bin c13_3_user.asm -o c13_user.bin
 python fixvhdwr.py c13_mbr.bin ~/VirtualBox\ VMs/LearnASM/LearnASM.vhd 0
 python fixvhdwr.py c13_core.bin ~/VirtualBox\ VMs/LearnASM/LearnASM.vhd 1
 python fixvhdwr.py c13_user.bin ~/VirtualBox\ VMs/LearnASM/LearnASM.vhd 50
+python fixvhdwr.py c13_diskdata.txt ~/VirtualBox\ VMs/LearnASM/LearnASM.vhd 100
