6bc0834b893df24a68607cd82859f59fcd2157a7
Locke <moyerock@gmail.com>
2019-06-25 23:43:18 +0800

    feat: construct from old ones

--- code/coq/test.v
+++ code/coq/test.v
@@ -56,3 +56,18 @@ Inductive color : Type :=
   | black
   | white
   | primary (p : rgb).
+
+Definition monochrome (c : color) : bool :=
+  match c with
+  | black => true
+  | white => true
+  | primary q =>false
+  end.
+
+Definition isred (c : color) : bool :=
+  match c with
+  | black => false
+  | white => false
+  | primary red => true
+  | primary _ => false
+  end.
