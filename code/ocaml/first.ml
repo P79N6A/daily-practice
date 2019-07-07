2f16043aae417142c12d9bfeb2e6fefc739a8741
Locke <moyerock@gmail.com>
2019-07-07 20:54:44 +0800

    feat: ocaml in practice

--- code/ocaml/first.ml
+++ code/ocaml/first.ml
@@ -1,2 +1,8 @@
-let average a b =
-  (a +. b) /. 2.0;;
+let rec iseven a =
+  if a = 0 then true
+  else if a - 1 == 0 then false
+  else iseven(a - 2);;
+
+
+let a = 12;;
+Printf.printf "iseven(%d) == %b\n" a (iseven 12) ;;
